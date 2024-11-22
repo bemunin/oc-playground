ff29e5c894543030053c884c8239b64a5b7defc8
bemunin <tw.munin@gmail.com>
2024-11-22 22:28:23 +0700

    Add register extension and update vscode settings extraPaths

--- foundationpose/sim.py
+++ foundationpose/sim.py
@@ -1,11 +1,17 @@
 # ruff:noqa: E402
+import logging
+import os
+
 import isaacsim  # noqa: F401
 from omni.isaac.kit import SimulationApp
 
 CONFIG = {"renderer": "RayTracedLighting", "headless": False}
 sim_app = SimulationApp(CONFIG)
 
+import carb
+import omni
 from omni.isaac.core import World
+from omni.isaac.core.utils import extensions
 
 
 def init_world():
@@ -17,6 +23,19 @@ def init_world():
     World(**world_settings)
     world = World.instance()
     world.initialize_physics()
+
+
+def register_extensions():
+    # add custom extension path
+    manager = omni.kit.app.get_app().get_extension_manager()
+    project_exts_path = f"{os.path.dirname(__file__)}/exts"
+    manager.add_path(project_exts_path)
+    print(f"foundationpose.sim: add this extension path to manager {project_exts_path}")
+
+    # enable required extensions
+    extensions.enable_extension("omni.kit.tool.measure")
+    extensions.enable_extension("omni.isaac.ros2_bridge")
+    extensions.enable_extension("oc.pg.foundationpose_scene")
 
 
 def running_sim(sim_app: SimulationApp):
@@ -35,6 +54,7 @@ def clear_world():
 
 if __name__ == "__main__":
     init_world()
+    register_extensions()
     running_sim(sim_app)
     clear_world()
     sim_app.close()
