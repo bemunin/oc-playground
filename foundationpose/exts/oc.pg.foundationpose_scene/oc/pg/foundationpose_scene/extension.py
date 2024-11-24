5873c58a28ffa1ec5ae04c4b323e8e4395efefa5
bemunin <tw.munin@gmail.com>
2024-11-24 16:13:06 +0700

    Add table and ycb objects in oc.pg.foundationpose_scene

--- foundationpose/exts/oc.pg.foundationpose_scene/oc/pg/foundationpose_scene/extension.py
+++ foundationpose/exts/oc.pg.foundationpose_scene/oc/pg/foundationpose_scene/extension.py
@@ -1,4 +1,5 @@
 import gc
+from typing import List, Optional
 
 import carb
 import numpy as np
@@ -7,7 +8,9 @@ from omni.isaac.core import World
 from omni.isaac.core.prims import XFormPrim
 from omni.isaac.core.utils import prims as prim_utils
 from omni.isaac.core.utils.nucleus import get_assets_root_path
+from omni.isaac.core.utils.rotations import euler_angles_to_quat
 from omni.isaac.core.utils.stage import add_reference_to_stage
+from pxr import PhysxSchema, UsdPhysics
 
 
 class Extension(omni.ext.IExt):
@@ -15,6 +18,7 @@ class Extension(omni.ext.IExt):
         self._ext_id = ext_id
         self._world = World.instance()
         self._scene = self._world.scene
+        self._asset_root = self._get_assets_root_path()
 
         self._setup_scene()
 
@@ -29,14 +33,9 @@ class Extension(omni.ext.IExt):
         gc.collect()
 
     def _setup_scene(self):
-        asset_root = self._get_assets_root_path()
         table_usd_path = (
-            asset_root
+            self._asset_root
             + "/Isaac/Environments/Outdoor/Rivermark/dsready_content/nv_content/common_assets/props_general/table01/table01.usd"
-        )
-
-        mustard_bottle_usd_path = (
-            asset_root + "/Isaac/Props/YCB/Axis_Aligned_Physics/006_mustard_bottle.usd"
         )
 
         self._scene.add_default_ground_plane()
@@ -65,21 +64,26 @@ class Extension(omni.ext.IExt):
 
         if not self._scene.object_exists("Table01"):
             add_reference_to_stage(usd_path=table_usd_path, prim_path="/World/Table01")
-            self._scene.add(XFormPrim("/World/Table01", name="Table01"))
+            xform = XFormPrim("/World/Table01", name="Table01")
+            UsdPhysics.CollisionAPI.Apply(xform.prim)
+            self._scene.add(xform)
 
-        if not self._scene.object_exists("MustardBottle"):
-            add_reference_to_stage(
-                usd_path=mustard_bottle_usd_path,
-                prim_path="/World/MustardBottle",
-            )
-            self._scene.add(
-                XFormPrim(
-                    "/World/MustardBottle",
-                    name="MustardBottle",
-                    position=np.array([0, 0, 1]),
-                    scale=np.array([1, 1, 1]),
-                )
-            )
+        # Add YCB object
+        self._add_ycb_object(
+            "MustardBottle", "MustardBottle01", [-0.1, -0.1, 1], [0, 0, -90]
+        )
+        self._add_ycb_object(
+            "MustardBottle", "MustardBottle02", [0.2, -0.2, 1], [0, 0, -45]
+        )
+        self._add_ycb_object("Scissors", "Scissors01", [0.1, 0.1, 1])
+        self._add_ycb_object(
+            "TunaFishCan", "TunaFishCan01", [0.25, 0.1, 1], [-90, 0, 0]
+        )
+
+        self._add_ycb_object("Mug", "Mug01", [-0.2, 0.1, 1], [-90, 0, 0])
+
+        self._add_ycb_object("LargeMarker", "LargeMarker01", [0, 0.1, 1], [0, 0, 0])
+        self._world.reset()
 
         return
 
@@ -89,3 +93,55 @@ class Extension(omni.ext.IExt):
             # Use carb to log warnings, errors and infos in your application (shown on terminal)
             carb.log_error("Could not find nucleus server with /Isaac folder")
         return assets_root_path
+
+    def _add_ycb_object(
+        self,
+        object: str,
+        name: str,
+        position: List[float],
+        rotation_deg: Optional[List[float]] = None,
+    ):
+        if self._scene.object_exists(name):
+            return
+
+        ycb_base_path = self._asset_root + "/Isaac/Props/YCB/Axis_Aligned/"
+        object_path_map = {
+            "MustardBottle": ycb_base_path + "006_mustard_bottle.usd",
+            "Scissors": ycb_base_path + "037_scissors.usd",
+            "TunaFishCan": ycb_base_path + "007_tuna_fish_can.usd",
+            "Mug": ycb_base_path + "025_mug.usd",
+            "Banana": ycb_base_path + "011_banana.usd",
+            "Bowl": ycb_base_path + "024_bowl.usd",
+            "SugarBox": ycb_base_path + "004_sugar_box.usd",
+            "LargeMarker": ycb_base_path + "040_large_marker.usd",
+        }
+
+        usd_path = object_path_map[object]
+        prim_path = f"/World/{name}"
+
+        add_reference_to_stage(
+            usd_path=usd_path,
+            prim_path=prim_path,
+        )
+
+        rotation_quaternion = None
+        if rotation_deg is not None:
+            rotation_quaternion = euler_angles_to_quat(
+                np.array(rotation_deg), degrees=True
+            )
+
+        xform = XFormPrim(
+            prim_path,
+            name=name,
+            position=np.array(position),
+            scale=np.array([1, 1, 1]),
+            orientation=rotation_quaternion,
+        )
+
+        UsdPhysics.CollisionAPI.Apply(xform.prim)
+        UsdPhysics.MassAPI.Apply(xform.prim)
+        UsdPhysics.RigidBodyAPI.Apply(xform.prim)
+        rigid_api = PhysxSchema.PhysxRigidBodyAPI.Apply(xform.prim)
+        rigid_api.GetEnableCCDAttr().Set(True)
+
+        self._scene.add(xform)
